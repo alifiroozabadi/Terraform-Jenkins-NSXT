d8c1ecce3374d4cd2fe8559fd8251aed8c050d90
Ali Firoozabadi <105077900+alifiroozabadi@users.noreply.github.com>
2023-07-11 14:33:25 +0330

    Update main.tf
    use variable

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
 provider "nsxt" {
-  host                 = "192.168.149.21"
-  username             = "admin"
-  password             = "nsx@password"
-  allow_unverified_ssl = true
-  max_retries          = 2
+  host                 = var.nsxt_host
+  username             = var.nsxt_username
+  password             = var.nsxt_password
+  allow_unverified_ssl = var.nsxt_allow_unverified_ssl
+  max_retries          = var.nsxt_max_retries
 }
 
 resource "nsxt_policy_group" "NSXT-AD-List-GP" {
-  display_name = "NSXT-AD-List-GP"
+  display_name = var.policy_group_display_name
 
   criteria {
     ipaddress_expression {
-      ip_addresses = ["192.168.1.1", "192.168.1.2", "192.168.1.3", "192.168.1.4"]
+      ip_addresses = var.policy_group_ip_addresses
     }
   }
 }
